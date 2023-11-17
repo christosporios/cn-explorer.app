a1da4729581beff6377bd5be3b75a2ab2583e0ba
Christos Porios <christos.porios@gmail.com>
2023-11-17 14:47:46 -0500

    Initial commit

---
+++ src/app/db.ts
@@ -0,0 +1,26 @@
+'use server';
+import { Pool } from 'pg';
+import { format } from 'date-fns';
+import dotenv from 'dotenv';
+import { memoize } from 'lodash';
+
+// Database Client
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || '5432', 10),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+
+export async function lastRatingDate() {
+    const result = await pool.query('SELECT MAX(created_at_millis) FROM ratings');
+    const lastDate = parseInt(result.rows[0].max);
+    console.log('lastRatingDate', lastDate);
+
+    return new Date(lastDate);
+}
